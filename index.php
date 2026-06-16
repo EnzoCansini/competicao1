e7cd667726a113aca8774881af1ce9b04380a8cc
enzocansini <enzopixnu@gmail.com>
2026-06-16 16:59:51 -0300

    16/06

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ptbr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link rel="stylesheet" href="./assets/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header class=" flex justify-between p-3">
+        <img class="" src="./assets/img/logo.png" alt="logo">
+        <button class=" bg-yellow-400 rounded-lg pl-4 pr-4">Entrar</button>
+    </header>
+    <main class=" h-screen">
+        <img src="./assets/img/Rectangle 1.png" alt="">
+    </main>
+</body>
+</html>
