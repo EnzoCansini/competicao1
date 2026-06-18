78426379ec11cd1b4bf9b043762b7fe436c3b6f2
EnzoCansini <enzogabrieullll@gmail.com>
2026-06-18 14:19:39 -0300

    1806 1

--- index.php
+++ index.php
@@ -1,19 +1,67 @@
 <!DOCTYPE html>
 <html lang="ptbr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link href="./src/output.css" rel="stylesheet">
+    <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">!-->
     <link rel="stylesheet" href="./assets/style.css">
+    <link rel="stylesheet" href="./assets/icons/icons.css">
     <title>Document</title>
 </head>
+
 <body>
-    <header class=" flex justify-between p-3">
+    <header class=" flex justify-between p-3 items-center bg-white">
         <img class="" src="./assets/img/logo.png" alt="logo">
-        <button class=" bg-yellow-400 rounded-lg pl-4 pr-4">Entrar</button>
+        <button class=" bg-yellow-400 rounded-lg pl-4 pr-4 pt-0 pb-0 h-min text-2xl">Entrar</button>
     </header>
-    <main class=" h-screen">
+    <main class=" h-max">
         <img src="./assets/img/Rectangle 1.png" alt="">
+        <div class="textos m-3">
+            <h1>Quem somos</h1>
+            <p>A InovaVida Digital nasceu com a missão de transformar o cuidado com a saúde na terceira idade, tornando o acesso à informação, acompanhamento médico e prevenção de doenças mais simples, humano e acessível.
+                Desenvolvemos soluções digitais voltadas especialmente para idosos e suas famílias, ajudando no acompanhamento de doenças crônicas, na organização de tratamentos e no monitoramento de indicadores de saúde de forma prática e intuitiva. Nosso objetivo é reduzir barreiras tecnológicas e aproximar as pessoas de um cuidado contínuo e preventivo.
+                Acreditamos que tecnologia e bem-estar devem caminhar juntos. Por isso, criamos uma plataforma pensada para facilitar o dia a dia, oferecendo acesso a conteúdos confiáveis sobre saúde, lembretes importantes, acompanhamento personalizado e mais autonomia para quem busca viver com qualidade de vida.
+                Mais do que uma startup de tecnologia, somos uma iniciativa dedicada a promover longevidade saudável, cuidado preventivo e inclusão digital para a melhor idade.</p>
+        </div>
+        <img src="./assets/img/Rectangle 3.png" alt="">
+        <div class="textos">
+            
+        </div>
+        <h1>Oferecemos</h1>
+        <div class="section grid grid-cols-2 gap-2 items-baseline justify-center pl-4 pr-4">
+            <div class="quadro place-items-center">
+                <div class="icon">
+                    <span class="fa6-solid--house-medical"></span>
+                </div>
+                <div class="texto text-center">Lorem ipsum dolor sit amet </div>
+            </div>
+            <div class="quadro place-items-center">
+                <div class="icon">
+                    <span class="uil--heart-medical"></span>
+                </span></div>
+                <div class="texto text-center">Lorem ipsum dolor sit amet </div>
+            </div>
+            <div class="quadro place-items-center">
+                <div class="icon">
+                    <span class="wpf--medical-doctor"></span>
+                </div>
+                <div class="texto text-center">Lorem ipsum dolor sit amet </div>
+            </div>
+            <div class="quadro place-items-center">
+                <div class="icon">
+                    <span class="mdi--youtube"></span>
+                </div>
+                <div class="texto text-center">Lorem ipsum dolor sit amet </div>
+            </div>
+        </div>
+        <img src="./assets/img/Rectangle 5.png" alt="">
+        <h1>Conteudo recomendado</h1>
+        <div class="section">
+
+        </div>
     </main>
 </body>
+
 </html>
